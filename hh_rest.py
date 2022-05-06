c900d13bed22bf196610738779752a1bce1b35a5
Mary True <mat-vsuwt@ya.ru>
2022-05-06 10:36:21 +0300

    flask

--- hh_rest.py
+++ hh_rest.py
@@ -1,56 +1,53 @@
 import requests
-import pprint
-import json
-import time
-url = 'https://api.hh.ru/vacancies'
 
-d=dict()
-d['keywords']= 'C#'
-requirements=dict()
 
-sal=0
-for p in range(3):
-    params = {
-        'text': 'C#',
-        'area': 66,
-        'only_with_salary': True,
-        'page': p # есть страницы т.к. данных много
-        }
+def parse(text):
+    url = 'https://api.hh.ru/vacancies'
 
-    result = requests.get(url, params=params).json()
+    d=dict()
+    d['keywords']= text
+    requirements=dict()
 
-    d['count']=result['found']
-    items = result['items']
-    #print(p)
+    sal=0
+    for p in range(3):
+        params = {
+            'text': text,
+            'area': 66,
+            'only_with_salary': True,
+            'page': p # есть страницы т.к. данных много
+            }
 
-    for item in items:
-        #print(item['name'])
-        urlv = item['url']
-        res = requests.get(urlv).json()
-        if res['key_skills']:
-            for req in res['key_skills']:
-                #print(req)
-                r= req['name']
+        result = requests.get(url, params=params).json()
 
-                if r in requirements:
+        d['count']=result['found']
+        items = result['items']
+        #print(p)
 
-                    requirements[r] += 1
+        for item in items:
+            #print(item['name'])
+            urlv = item['url']
+            res = requests.get(urlv).json()
+            if res['key_skills']:
+                for req in res['key_skills']:
+                    #print(req)
+                    r= req['name']
 
-                else:
-                    requirements[r] =1
-        if item['salary']['from']:
-            #print(result['items'][i]['salary']['from'])
-            sal+=item['salary']['from']
-        elif item['salary']['to']:
-            #print(result['items'][i]['salary']['to'])
-            sal+=item['salary']['to']
-    #pprint.pprint(d)
-    #time.sleep(1)
-d['salary']=sal/d['count']
-d['requirements']=sorted(requirements.items(), key=lambda q: q[1],reverse=True)
-# d['requirements'] = requirements
-print('----------------------')  
-pprint.pprint(d)
-print('----------------------') 
-with open('hh.json', 'w') as f:
-       json.dump(d, f)
+                    if r in requirements:
+
+                        requirements[r] += 1
+
+                    else:
+                        requirements[r] = 1
+            if item['salary']['from']:
+
+                sal += item['salary']['from']
+            elif item['salary']['to']:
+
+                sal += item['salary']['to']
+    if d['count'] != 0:
+        d['salary'] = sal/d['count']
+    else:
+        d['salary'] = sal
+    d['requirements'] = sorted(requirements.items(), key = lambda q: q[1], reverse = True)
+
+    return d
